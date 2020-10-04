35cb31653394e7403898f47a9973773c8f12e7fe
Kaustav Mukherjee <kaustav@Kaustavs-MacBook-Pro.local>
2020-10-05 00:44:11 +0530

    Initial Commit

---
+++ Ch08/ex02/ftp/ftp.go
@@ -0,0 +1,17 @@
+package ftp
+
+import (
+	"fmt"
+	"log"
+)
+
+func Serve(c Conn) {
+	c.respond("220 Service is ready")
+}
+func (c *Conn) respond(s string) {
+	log.Print(">> ", s)
+	_, err := fmt.Fprint(c.conn, s, c.eol())
+	if err != nil {
+		log.Print(err)
+	}
+}
